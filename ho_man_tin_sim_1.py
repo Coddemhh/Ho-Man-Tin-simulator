12db23338eb5776c077910ab2a03dd5c0e907cf1
cofebro <hr18lpt@gmail.com>
2025-12-19 22:44:25 +0800

    added rolling

--- ho_man_tin_sim_1.py
+++ ho_man_tin_sim_1.py
@@ -4,6 +4,25 @@ import random
 import math
 import os 
 
+colors = {
+    "C": "\033[39m",
+    "U": "\033[32m", 
+    "R": "\033[36m",  
+    "E": "\033[35m",  
+    "L": "\033[33m", 
+    "RESET": "\033[0m"  
+}
+
+rarity = {
+    1: "C",
+    2: "U",
+    3: "R",
+    4: "E",
+    5: "L"
+}
+
+chances = [1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,3,3,3,3,4,4,5]
+
 class labubu:
     def __init__(self, name, efficiency, age, gender):
         self.name = name
@@ -38,6 +57,26 @@ class labubu:
 
     def sextransfer(self):
         self.gender = "M" if self.gender == "F" else self.gender = "F"     
+
+def rolling():
+    show = ['','','','','','','','','']
+    for i in range(len(show)):
+        a = random.randint(0,len(chances)-1)
+        b = chances[a]
+        if b in rarity:
+            show[i] = rarity[b]
+    print("Rolling for your labubu......")
+    print('                ↓')
+    for i in range(50):
+        c_show = [f"{colors[char]}{char}{colors['RESET']}" if char in colors else char for char in show]
+        print('   '.join(c_show), end='\r')
+        time.sleep(i/70)
+        show = show[-1:] + show[:-1]
+        a = random.randint(0,len(chances)-1)
+        b = chances[a]
+        if b in rarity:
+            show[0] = rarity[b]
+    return show[4]
 
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
